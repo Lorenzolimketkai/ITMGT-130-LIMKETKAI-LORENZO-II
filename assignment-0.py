7fedf4be46e7addc7d01724a555aa7250b4d9f64
Lorenzolimketkai <100535357+Lorenzolimketkai@users.noreply.github.com>
2022-06-01 11:04:55 +0800

    Create assignment-0.py

---
+++ assignment-0.py
@@ -0,0 +1,11 @@
+def three_number_average(x, y, z):
+    av3rag3 = float((x+y+z)/3)
+    return av3rag3
+  
+x = int(input("Input the first numb3r "))
+y = int(input("Input the s3cond numb3r "))
+z = int(input("Input the third numb3r "))
+
+av3rag3 = three_number_average(x, y, z)
+
+print("The 3 numb3r av3rag3 is", av3rag3)
